72b33de0a6b932714798b1950778c18ac9ab95b2
Jason <ozil.liu2@gmail.com>
2017-11-16 21:47:04 +0800

    Master

--- src/main/java/com/grocery/configuration/GroceryConfiguration.java
+++ src/main/java/com/grocery/configuration/GroceryConfiguration.java
@@ -3,11 +3,13 @@ package com.grocery.configuration;
 import com.grocery.interceptor.GroceryInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.servlet.ServletRequestListener;
@@ -16,7 +18,7 @@ import javax.servlet.ServletRequestListener;
  * Created by Jason on 14/10/2017.
  */
 
-@Component
+@Configuration
 public class GroceryConfiguration extends WebMvcConfigurerAdapter{
 
     @Autowired
@@ -24,8 +26,8 @@ public class GroceryConfiguration extends WebMvcConfigurerAdapter{
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        super.addInterceptors(registry);
         registry.addInterceptor(groceryInterceptor);
+        super.addInterceptors(registry);
     }
 
     @Bean
@@ -33,7 +35,14 @@ public class GroceryConfiguration extends WebMvcConfigurerAdapter{
         return new RequestContextListener();
     }
 
-//    @Bean(name = "multipartResolver")
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        super.addViewControllers(registry);
+    }
+
+    //    @Bean(name = "multipartResolver")
     public MultipartResolver multipartResolver(){
         CommonsMultipartResolver resolver = new CommonsMultipartResolver();
         resolver.setDefaultEncoding("UTF-8");
