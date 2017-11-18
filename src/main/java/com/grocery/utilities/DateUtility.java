b2df0ca3dcd4b07519e11b71b9fe21c2cf0fc0f6
Jason <ozil.liu2@gmail.com>
2017-11-18 22:05:03 +0800

    Master

--- src/main/java/com/grocery/utilities/DateUtility.java
+++ src/main/java/com/grocery/utilities/DateUtility.java
@@ -1,16 +1,42 @@
 package com.grocery.utilities;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Jason on 14/10/2017.
  */
 public class DateUtility {
-    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private static Map<DateType, ThreadLocal<SimpleDateFormat>> simpleDataFormatThreadLocal = new HashMap<>();
 
-    public static String getCurrentDate () {
-        Date currentdate = new Date();
-        return simpleDateFormat.format(currentdate);
+    protected static SimpleDateFormat getSimpleDateFormat(DateType dateType) {
+        ThreadLocal<SimpleDateFormat> threadLocal = simpleDataFormatThreadLocal.get(dateType);
+
+        if (threadLocal == null) {
+            synchronized (simpleDataFormatThreadLocal) {
+                threadLocal = simpleDataFormatThreadLocal.get(dateType);
+                if (threadLocal == null) {
+                    threadLocal = ThreadLocal.withInitial(() -> new SimpleDateFormat(dateType.getType()));
+                    simpleDataFormatThreadLocal.put(dateType, threadLocal);
+                }
+            }
+        }
+
+        return threadLocal.get();
+    }
+
+    public static Date getFormattedDateByString(DateType dateType, String date) throws ParseException {
+        return getSimpleDateFormat(dateType).parse(date);
+    }
+
+    public static String getFormattedDateByDate(DateType dateType, Date date) {
+        return getSimpleDateFormat(dateType).format(date);
+    }
+
+    public static String getCurrentDate() {
+        return getFormattedDateByDate(DateType.YMDHMS_, new Date());
     }
 }
