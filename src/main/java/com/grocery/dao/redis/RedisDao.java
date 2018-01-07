0b2e5b706d81bf6303dbd6727277a68b39b7a2b4
Jason <ozil.liu2@gmail.com>
2018-01-07 22:22:59 +0800

    add Redis

---
+++ src/main/java/com/grocery/dao/redis/RedisDao.java
@@ -0,0 +1,14 @@
+package com.grocery.dao.redis;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RedisDao {
+
+    private final Logger logger = LoggerFactory.getLogger(RedisDao.class);
+
+
+
+}
